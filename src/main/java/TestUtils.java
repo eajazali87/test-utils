7cbf968b0bf7a000962688d3414325e0447c6cbd
umahaea <eajazali.mahaboobbasha@pearson.com>
2018-11-26 15:28:36 -0700

    add more fixes

--- src/main/java/TestUtils.java
+++ src/main/java/TestUtils.java
@@ -1,5 +1,3 @@
-import org.testng.annotations.Test;
-import org.testng.reporters.Files;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -7,31 +5,23 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.*;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+
+
 /**
- * The RemoveConfigNodes program implements an logic that does the below operations:
- * 1. Remove test-method nodes where status = "Skip"
- * 2. Remove test-method nodes whose "is-config" attribute value is "true". This removes things like beforeClass, afterClass, setUp etc.
- * 3. In each test-method node, append a unique number [1],[2],[3], etc. to each name attribute. This forces ALM to bring these in a separate tests, otherwise
- * ALM will bring these in as different runs for the same test
- *
  * @author Eajaz
+ * Created on 4/18/17
  */
 
 public class TestUtils {
 
-    protected static DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    protected static DocumentBuilder builder;
+    protected static DocumentBuilder builder = null;
 
     protected static Document doc = null;
 
@@ -39,19 +29,25 @@ public class TestUtils {
     protected static Transformer t = null;
 
 
+    /**
+     * The removeConfigNodes method implements an logic that does the below operations:
+     * 1. Remove test-method nodes where status = "Skip"
+     * 2. Remove test-method nodes whose "is-config" attribute value is "true". This removes things like beforeClass, afterClass, setUp etc.
+     * 3. In each test-method node, append a unique number [1],[2],[3], etc. to each name attribute (If you use ALM, then this forces ALM to bring these in a separate tests, otherwise ALM will bring these in as different runs for the same test
+     */
     public static void removeConfigNodes(String inputFilePath) {
 
         try {
             doc = builder.parse(inputFilePath);
         } catch (SAXException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
         File f = new File(inputFilePath);
         if (f.exists()) {
         } else {
-            System.out.println("file not found");
+            System.out.println("file not found, please check if your input file is in the correct path");
             System.exit(1);
         }
 
@@ -60,9 +56,8 @@ public class TestUtils {
         List<Element> removeElements = new LinkedList<Element>(); // a List for removing nodes
         List<Element> retainElements = new LinkedList<Element>(); // a List for retaining nodes
 
-        /*
-         Iterate over and find out <test-method> node with attribute "is-config" equals "true" or "status" equals "SKIP"
-         */
+
+        //Iterate over and find out <test-method> node with attribute "is-config" equals "true" or "status" equals "SKIP"
         for (int i = 0; i < testMethodNodeList.getLength(); i++) {
             Element e = (Element) testMethodNodeList.item(i);
             if ((e.getAttribute("is-config").equals("true")) || e.getAttribute("status").equals(
@@ -98,9 +93,16 @@ public class TestUtils {
         System.out.println("Total no. of valid <test-methods> : " + retainElements.size());
 
         //Write it to a file the final testng.xml file
-        t = tf.newTransformer();
-        t.transform(new DOMSource(doc), new StreamResult(new File(inputFilePath)));
-
+        try {
+            t = tf.newTransformer();
+        } catch (TransformerConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            t.transform(new DOMSource(doc), new StreamResult(new File(System.getProperty("user.dir")+"output.xml")));
+        } catch (TransformerException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
